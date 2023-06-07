9e6dd511338fed02bd60c8267a867352abe38097
hikaru8079 <hikaru8079@outlook.jp>
2023-06-07 12:20:22 +0900

    add:package

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment()){
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+app.UseStatusCodePages();
+app.UseFileServer();
 app.UseHttpsRedirection();
+app.UseHttpLogging();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
