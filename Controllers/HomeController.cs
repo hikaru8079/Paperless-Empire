113c2aa479eb3cfe09958290ab19c8d3b4bd745d
hikaru8079 <hikaru8079@outlook.jp>
2023-06-13 09:51:39 +0900

    test.auth

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Paperless_Empire.Models;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
@@ -16,10 +17,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 
 namespace Paperless_Empire.Controllers;
 
-public class HomeController : Controller
+/*public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
@@ -37,27 +39,55 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-    [HttpGet]
-    [Authorize]
-    public async Task<IActionResult> GetEmailAddress()
+}*/
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
     {
-        var accessToken = HttpContext.Request.Headers["X-MS-TOKEN-GOOGLE-ACCESS-TOKEN"];
+        // Googleアカウントの情報を取得
+        GoogleUserInfo userInfo = GetGoogleUserInfoFromHeaders(Request).Result;
 
-        var httpClient = new HttpClient();
-        var requestUrl = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken;
-
-        var response = await httpClient.GetAsync(requestUrl);
-        if (response.IsSuccessStatusCode)
+        // ビューモデルを作成し、Googleアカウントの情報を設定
+        var viewModel = new GoogleAccountViewModel
         {
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var jsonObject = JObject.Parse(responseContent);
-            var email = jsonObject.Value<string>("email");
+            Name = userInfo.Name,
+            Email = userInfo.Email,
+            // 他のプロパティも必要な場合は設定
+        };
 
-            return Ok(email);
-        }
-        else
+        return View(viewModel);
+    }
+
+    public async Task<GoogleUserInfo> GetGoogleUserInfoFromHeaders(HttpRequest request)
+    {
+        var idToken = request.Headers["X-MS-TOKEN-GOOGLE-ID-TOKEN"].FirstOrDefault();
+        var accessToken = request.Headers["X-MS-TOKEN-GOOGLE-ACCESS-TOKEN"].FirstOrDefault();
+
+        using (var httpClient = new HttpClient())
         {
-            return BadRequest("Failed to retrieve email address from Google.");
+            var endpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+            var userInfoRequest = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            userInfoRequest.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+            var userInfoResponse = await httpClient.SendAsync(userInfoRequest);
+            if (userInfoResponse.IsSuccessStatusCode)
+            {
+                var content = await userInfoResponse.Content.ReadAsStringAsync();
+                var userInfo = JsonConvert.DeserializeObject<GoogleUserInfo>(content);
+                return userInfo;
+            }
+            else
+            {
+                throw new Exception("Google API request failed.");
+            }
         }
     }
+}
+
+public class GoogleUserInfo
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    // 他のプロパティも必要な場合は追加
 }
---
+++ Models/GoogleAccountViewModel.cs
@@ -0,0 +1,6 @@
+public class GoogleAccountViewModel
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    // 他のプロパティも必要な場合は追加
+}
