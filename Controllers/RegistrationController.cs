36c8208257ced049918680d7cf47fa43183b791b
Hikaru Oga <hikaru8079@outlook.jp>
2023-06-30 15:10:52 +0900

    add:az slack

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -1,6 +1,8 @@
+using Paperless_Empire.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
-using Paperless_Empire.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 namespace Paperless_Empire.Controllers;
 public class RegistrationController : Controller{
     public IActionResult Index(){
@@ -9,24 +11,32 @@ public class RegistrationController : Controller{
 
     [HttpPost]
     public IActionResult Check(RegistrationModel Model){
+        //完了ボタンを押すとモデルの値が全部nullになるのでセッションに入力情報を格納
+        var modelData = JsonConvert.SerializeObject(Model);
+        HttpContext.Session.SetString("RegistrationModel", modelData);
+        //
         return View(Model);
     }
-    public async Task<IActionResult> Complete(RegistrationModel Model){
-        //ここにデータベース追加処理を書いてね
-        //多分値はViewDataを使って渡してあげればいいかも？
-        //無理なら変数に代入してあげるのもアリ
-        //終わったらページ表示させるように同期処理するか処理中ページを用意する
+    public async Task<IActionResult> Complete(RegistrationModel Model,IConfiguration _configuration){
+        var modelData = HttpContext.Session.GetString("RegistrationModel");
+        var model = JsonConvert.DeserializeObject<RegistrationModel>(modelData!);
+        /*
+        ↑より下で、Slack送信文の上にデータベース追加処理を書いてね
+        これとSlack送信が終わったらページ表示させるように同期処理するか処理中ページを用意する
+        */
         //↓教師へ承認案内Slack通知送信
-        var Number = Model.Number;
-        var Name = Model.Name;
-        var Date = Model.Date;
         var Client = new HttpClient();
-        string token = "xoxb-5471836121122-5504690798032-1owJCC6U5C28HnA6DDl2sllU";
+        var tokenaz = _configuration.GetConnectionString("SLACK_TOKEN");
+
         string channel = "#slackapi-test";
-        string text = $"【公欠届承認案内】\n新着の公欠届があります。\n\n----概要----\n学籍番号: {Number}\n氏名: {Name}\n公欠日: {Date}\n\n詳細をこちらのURLから承認ページへアクセスし、承認/却下操作を行ってください。\nhttps://paperless-empire.site/TeachingStaff";
-        var data = new StringContent($"token={token}&channel={channel}&text={text}", Encoding.UTF8, "application/x-www-form-urlencoded");
+        var text = $"【公欠届承認案内】\n新着の公欠届があります。\n\n----概要----\n学籍番号: {model!.Number}\n氏名: {model.Name}\n公欠日: {model.Date}\n\n詳細をこちらのURLから承認ページへアクセスし、承認/却下操作を行ってください。\nhttps://paperless-empire.site/TeachingStaff";
+        var data = new StringContent($"token={tokenaz}&channel={channel}&text={text}",Encoding.UTF8,"application/x-www-form-urlencoded");
         var response = await Client.PostAsync("https://slack.com/api/chat.postMessage", data);
         var responseInString = await response.Content.ReadAsStringAsync();
+        var ParsedR = JObject.Parse(responseInString);
+        Console.WriteLine(ParsedR);
+        //最後にセッションから値を削除
+        HttpContext.Session.Remove("RegistrationModel");
         return View();
     }
 }
