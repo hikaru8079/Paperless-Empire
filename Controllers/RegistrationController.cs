03152264424a034f72022526f8deab72f58a88a9
Hikaru Oga <hikaru8079@outlook.jp>
2023-06-30 15:43:37 +0900

    test:slack

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -2,9 +2,16 @@ using Paperless_Empire.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
+using System.Configuration;
+
 namespace Paperless_Empire.Controllers;
 public class RegistrationController : Controller{
+    private IConfiguration _configuration;
+    public RegistrationController(IConfiguration configuration){
+            _configuration = configuration;
+    }
     public IActionResult Index(){
         return View();
     }
@@ -17,7 +24,7 @@ public class RegistrationController : Controller{
         //
         return View(Model);
     }
-    public async Task<IActionResult> Complete(RegistrationModel Model,IConfiguration _configuration){
+    public async Task<IActionResult> Complete(RegistrationModel Model){
         var modelData = HttpContext.Session.GetString("RegistrationModel");
         var model = JsonConvert.DeserializeObject<RegistrationModel>(modelData!);
         /*
@@ -26,11 +33,10 @@ public class RegistrationController : Controller{
         */
         //↓教師へ承認案内Slack通知送信
         var Client = new HttpClient();
-        var tokenaz = _configuration.GetConnectionString("SLACK_TOKEN");
-
+        var token = _configuration.GetConnectionString("SLACK_TOKEN");
         string channel = "#slackapi-test";
         var text = $"【公欠届承認案内】\n新着の公欠届があります。\n\n----概要----\n学籍番号: {model!.Number}\n氏名: {model.Name}\n公欠日: {model.Date}\n\n詳細をこちらのURLから承認ページへアクセスし、承認/却下操作を行ってください。\nhttps://paperless-empire.site/TeachingStaff";
-        var data = new StringContent($"token={tokenaz}&channel={channel}&text={text}",Encoding.UTF8,"application/x-www-form-urlencoded");
+        var data = new StringContent($"token={token}&channel={channel}&text={text}",Encoding.UTF8,"application/x-www-form-urlencoded");
         var response = await Client.PostAsync("https://slack.com/api/chat.postMessage", data);
         var responseInString = await response.Content.ReadAsStringAsync();
         var ParsedR = JObject.Parse(responseInString);
