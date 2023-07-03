d961e9d39dd2d657f69d8f3ca978ff202727c43c
t-tamai <g020c1040@g.neec.ac.jp>
2023-07-03 11:06:04 +0900

    add:report

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -9,9 +9,15 @@ public class ReportController : Controller
     {
         return View();
     }
-    public IActionResult Check()
-    {
-        return View();
+    public IActionResult Check(ReportViewModel model){
+        ViewData["Address"] = Request.Form["Address"];
+        ViewData["AptitudeTestContent"] = Request.Form["AptitudeTestContent"];
+        ViewData["CommonSenseContent"] = Request.Form["CommonSenseContent"];
+        ViewData["SpecialityContent"] = Request.Form["SpecialityContent"];
+        ViewData["EssayContent"] = Request.Form["EssayContent"];
+        ViewData["InterviewContent"] = Request.Form["InterviewContent"];
+        ViewData["Advice"] = Request.Form["Advice"];
+        return View(model);
     }
     public IActionResult Entry()
     {
---
+++ Models/ReportViewModel.cs
@@ -0,0 +1,18 @@
+namespace Paperless_Empire;
+public class ReportViewModel{
+    public bool Resume { get; set; }
+    public bool Score { get; set; }
+    public bool Graduation { get; set; }
+    public bool Health { get; set; }
+    public bool Entry { get; set; }
+    public bool Recommend { get; set; }
+    public bool Introduce { get; set; }
+    public bool DocOthers { get; set; }
+    public bool AptitudeTest { get; set; }
+    public bool CommonSense { get; set; }
+    public bool Speciality { get; set; }
+    public bool Essay { get; set; }
+    public bool TestOthers { get; set; }
+    public bool Personal { get; set; }
+    public bool Group { get; set; }
+}
